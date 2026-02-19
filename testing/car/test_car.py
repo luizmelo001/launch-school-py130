bea366c97ce06c1985fef9547edebc2a5deb5ed9
Luiz Melo <luizmelo001@gmail.com>
2026-02-19 16:23:56 -0300

    Testing examples

--- testing/car/test_car.py
+++ testing/car/test_car.py
@@ -2,9 +2,37 @@ import unittest
 from car import Car
 
 class CarTest(unittest.TestCase):
+    def test_car_exists(self):
+        car = Car()
+        self.assertTrue(car is not None)
+
     def test_wheels(self):
         car = Car()
         self.assertEqual(4, car.wheels)
 
+    def test_name_is_none(self):
+        car = Car()
+        self.assertIsNone(car.name)
+
+    def test_instance_of_car(self):
+        car = Car()
+        self.assertIsInstance(car, Car)
+
+    def test_includes_car(self):
+        car = Car()
+        arr = [1, 2, 3]
+        arr.append(car)
+        self.assertIn(car, arr)
+
+    @unittest.skip("Skipping this test for now")
+    def test_bad_wheels(self):
+        car = Car()
+        self.assertEqual(3, car.wheels)
+
+    def test_set_name_raises(self):
+        car = Car()
+        self.assertRaises(TypeError, setattr, car, 'name', 1234)
+    
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
